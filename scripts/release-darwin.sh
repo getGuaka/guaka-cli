7cbba4f93c5def6f328b7d8213bbcddb21d1f232
Thi <t@thi.im>
2018-08-25 22:39:26 +0900

    Remove commented out code

--- scripts/release-darwin.sh
+++ scripts/release-darwin.sh
@@ -1,10 +1,6 @@
-
 source ./scripts/release-common.sh
 
 [ -z ${VERSION+x} ] && { echo "VERSION is missing"; exit 1; }
 [ -z ${GITHUB_TOKEN+x} ] && { echo "GITHUB_TOKEN is missing"; exit 1; }
-
-# git tag $VERSION
-# git push origin master --tags
 
 release $VERSION "darwin" "bin/darwin/guaka" $GITHUB_TOKEN
