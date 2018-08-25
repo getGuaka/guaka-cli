e8ee9cc3ca2f5e7750e606c81d141f9b08e6cb7f
Thi <t@thi.im>
2018-08-25 23:58:58 +0900

    Remove github-release

--- scripts/github-release.sh
+++
@@ -1,4 +0,0 @@
-[ -z ${VERSION+x} ] && { echo "VERSION is missing"; exit 1; }
-[ -z ${GITHUB_TOKEN+x} ] && { echo "GITHUB_TOKEN is missing"; exit 1; }
-
-github-release release --user getGuaka --security-token ${GITHUB_TOKEN} --repo guaka-cli --tag ${VERSION} --name "Version ${VERSION}"
