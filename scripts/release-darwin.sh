27f4214cdf2de46c309ba77e09cb86ca3840bbb1
Thi <t@thi.im>
2018-08-26 00:24:43 +0900

    Rename tarball and remove GITHUB_TOKEN

--- scripts/release-darwin.sh
+++ scripts/release-darwin.sh
@@ -1,6 +1,5 @@
 source ./scripts/release-common.sh
 
 [ -z ${VERSION+x} ] && { echo "VERSION is missing"; exit 1; }
-[ -z ${GITHUB_TOKEN+x} ] && { echo "GITHUB_TOKEN is missing"; exit 1; }
 
 release $VERSION "darwin" "bin/darwin/guaka" $GITHUB_TOKEN
--- scripts/release-linux.sh
+++ scripts/release-linux.sh
@@ -1,6 +1,5 @@
 source ./scripts/release-common.sh
 
 [ -z ${VERSION+x} ] && { echo "VERSION is missing"; exit 1; }
-[ -z ${GITHUB_TOKEN+x} ] && { echo "GITHUB_TOKEN is missing"; exit 1; }
 
 release $VERSION "linux" "bin/linux/guaka" $GITHUB_TOKEN
